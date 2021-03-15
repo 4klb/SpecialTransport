1a6b99c141ebcb63be834208f0b74cb8994226b2
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-03-15 22:12:36 +0600

    Добавил кдасс Task  и User

---
+++ SpecialTransport/Models/IdTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdUserRecovery.TaskUser
+{
+    public class IdTask
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+    }
+}
---
+++ SpecialTransport/Models/IdUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdUserRecovery.TaskUser
+{
+    public class IdUser
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Data { get; set; }
+    }
+}
