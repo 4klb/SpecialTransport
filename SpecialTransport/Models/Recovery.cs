7817b53f5dac6359d2eae7b4735461727c1df41e
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2021-03-15 23:31:08 +0600

    Расскомментировали

--- SpecialTransport/Models/Recovery.cs
+++ SpecialTransport/Models/Recovery.cs
@@ -1,12 +1,12 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace SpecialTransport.Models
-//{
-//    public class Recovery : Entity
-//    {
-//        public User UserId { get; set; }
-//        public User User { get; set; }
-//    }
-//}
+namespace SpecialTransport.Models
+{
+    public class Recovery : Entity
+    {
+        public User UserId { get; set; }
+        public User User { get; set; }
+    }
+}
